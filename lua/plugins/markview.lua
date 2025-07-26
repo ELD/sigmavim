4a69ee7ae11522830b6a9e4e077d8d583dea0d66
Eric Dattore <eric.dattore@sumologic.com>
2025-07-26 11:11:07 -0600

    Switch from Avante to OpenCode

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -3,4 +3,15 @@ return {
 	"OXY2DEV/markview.nvim",
 	lazy = false,
 	priority = 49,
+	keys = {
+		{ "<leader>mci", "<cmd>Checkbox Interactive<cr>", desc = "Markview Checkbox Interactive" },
+		{ "<leader>mct", "<cmd>Checkbox toggle<cr>", desc = "Markview Checkbox Toggle" },
+		{ "<leader>mcc", "<cmd>Editor create<cr>", desc = "Markview Create Code Block" },
+		{ "<leader>mce", "<cmd>Editor edit<cr>", desc = "Markview Edit Code Block" }
+	},
+	config = function()
+		require("markview.extras.checkboxes").setup()
+		require("markview.extras.editor").setup()
+		require("markview.extras.headings").setup()
+	end,
 };
